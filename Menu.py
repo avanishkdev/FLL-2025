40d6bab0ef3346f5d993a2fe47b139baf1eddfe0
Avanish Kumar <66087777+avanishkdev@users.noreply.github.com>
2025-09-22 19:50:28 -0400

    Add files via upload

--- Menu.py
+++ Menu.py
@@ -1,12 +1,10 @@
 from pybricks.tools import hub_menu
 
 # Make a menu to choose a letter. You can also use numbers.
-selected = hub_menu("H", "S", "L")
+selected = hub_menu("3", "1")
 
 # Based on the selection, run a program.
-if selected == "H":
-    import Hello
-elif selected == "S":
-    import Sound
-elif selected == "L":
-    import Light
+if selected == "3":
+    import Mission_3
+elif selected == "1":
+    import Mission_1
